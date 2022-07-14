6aeb05762f21f30129869be18721fb56a273e9e4
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-07-14 10:51:49 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,3 +6,15 @@ def find_all_odds(lst):
             odds_list.append(element)
 
     return odds_list
+
+
+
+
+def string_lengths(strings):
+    lengths = []
+
+    for string in strings:
+        length = len(string)
+        lengths.append(length)
+
+    return lengths
