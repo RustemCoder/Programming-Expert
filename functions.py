74b2ff3f94e6a0478dae9e3573fb3a6bacdce049
Rustem <48765033+RustemCoder@users.noreply.github.com>
2022-07-14 10:55:27 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -18,3 +18,29 @@ def string_lengths(strings):
         lengths.append(length)
 
     return lengths
+
+def compare_lists(lst1=[], lst2=[]):
+    lst1_set = set(lst1)
+    lst2_set = set(lst2)
+    set_intersection = lst1_set.intersection(lst2_set)
+
+    return len(set_intersection)
+
+def trim_list(lst, elements_to_trim):
+    trimmed_list = []
+
+    for idx in range(len(lst) - elements_to_trim):
+        element = lst[idx]
+        trimmed_list.append(element)
+
+    return trimmed_list
+
+def running_sums(numbers):
+    sums = []
+
+    current_sum = 0
+    for number in numbers:
+        current_sum += number
+        sums.append(current_sum)
+
+    return sums
